f3b520c6b3b224a4918ee55ea8b4eacce2a86850
Spencer Cheng <spencercheng@SC-MBP15-2018.local>
2018-07-21 23:11:17 -0700

    9-react_component: learning how to import component to another component

---
+++ 0x00-intro/9-react_component/src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Title from './title.jsx';
+
+export default class App extends React.Component {
+	render() {
+		return(
+      <Title />
+		)
+	}
+};
---
+++ 0x00-intro/9-react_component/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Holberton School webpack',
+      template: './src/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  watchOptions: {
+    poll: 1000
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+};
