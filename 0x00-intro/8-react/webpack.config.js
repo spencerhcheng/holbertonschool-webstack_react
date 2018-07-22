67289a942ea2a09289ec1d8ea7f2446490888690
Spencer Cheng <spencercheng@SC-MBP15-2018.local>
2018-07-21 22:55:42 -0700

    8-react: edit config files

--- 0x00-intro/8-react/webpack.config.js
+++ 0x00-intro/8-react/webpack.config.js
@@ -1,31 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack')
+const webpack = require('webpack');
 
 module.exports = {
-	entry: './src/index.js',
-	devServer: {
-		content: './dist',
-		hot: true
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'Holberton School webpack',
-			tempalte: './src/index.html'
-		}),
-		new webpack.HotModuleReplacementPlugin()
-	],
-	watchOptions: {
-		poll: 1000
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.bundle.js',
-	},
-	module: {
-		loaders: [
-			{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-			{ test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-		]
-	}
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Holberton School webpack',
+      template: './src/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  watchOptions: {
+    poll: 1000
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
 };
