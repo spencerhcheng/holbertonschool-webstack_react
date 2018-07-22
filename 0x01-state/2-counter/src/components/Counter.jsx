53637de53cbfd301ea66aec242965f08384f46ae
Spencer Cheng <spencercheng@SC-MBP15-2018.local>
2018-07-22 15:31:35 -0700

    2-counter: initialize state value to 89

---
+++ 0x01-state/2-counter/src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 89 };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.value}
+      </div>
+    );
+  }
+}
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired
+};
+
+Counter.defaultProps = {
+  value: 0
+};
