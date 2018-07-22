78b759cc0473a8d33b9ef8ca9a11f02702b3afaa
Spencer Cheng <spencercheng@SC-MBP15-2018.local>
2018-07-22 15:32:13 -0700

    1-counter: passing prop counterValue

---
+++ 0x01-state/1-counter/src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Counter extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.value}
+      </div>
+    );
+  }
+}
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired
+};
+
+Counter.defaultProps = {
+  value: 0
+};
