110645c90d6dc172b3f9f6d45a49c2255bbcdb81
Spencer Cheng <spencercheng@SC-MBP15-2018.local>
2018-07-21 23:29:04 -0700

    9-react_component: learning about props

--- 0x00-intro/9-react_component/src/components/Title.jsx
+++ 0x00-intro/9-react_component/src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Title extends React.Component {
   render() {
     return (
-      <h1>Welcome to Holberton School</h1>
-    )
+      <h1>Welcome to {this.props.name}</h1>
+    );
   }
-};
+}
